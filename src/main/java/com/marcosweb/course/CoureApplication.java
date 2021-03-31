880906f6c5dffcd26e9c5f80757e7a33472e2853
Marcos Molitor Neto <marcos-molitor@hotmail.com>
2021-03-31 10:44:01 -0300

    Voltando a criar apps

---
+++ src/main/java/com/marcosweb/course/CoureApplication.java
@@ -0,0 +1,13 @@
+package com.marcosweb.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoureApplication.class, args);
+	}
+
+}
